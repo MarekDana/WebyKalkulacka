44e1db727090688739a291ecc37f1b861ea3fa85
MarekDana <43402311+MarekDana@users.noreply.github.com>
2018-11-07 11:04:29 +0100

    Upgrade

--- src/calc.js
+++ src/calc.js
@@ -1,6 +1,7 @@
 const calculator = {
   displayValue: "0",
   firstOperand: null,
+  secondOperand: null,
   waitingForSecondOperand: false,
   operator: null
 };
@@ -14,6 +15,7 @@ updateDisplay();
 
 function inputDigit(digit) {
   if (calculator.waitingForSecondOperand === true) {
+    console.log(digit);
     calculator.displayValue = digit;
     calculator.waitingForSecondOperand = false;
   } else {
@@ -28,26 +30,57 @@ function inputDigit(digit) {
 function handleOperator(nextOperator) {
   if (calculator.firstOperand == null) {
     calculator.firstOperand = parseFloat(calculator.displaValue);
+    console.log(calculator.firstOperand);
+  } else if (calculator.operator) {
+    const result = performCalculation[calculator.operator](
+      calculator.firstOperand,
+      parseFloat(calculator.displayValue)
+    );
+    calculator.displayValue = String(result);
+    calculator.firstOperand = result;
+    console.log(calculator.firstOperand);
   }
+
   calculator.waitingForSecondOperand = true;
   calculator.operator = nextOperator;
 }
+const performCalculation = {
+  "÷": (firstOperand, secondOperand) => firstOperand / secondOperand,
 
-const keys = document.querrySelector(".calculator-keys");
-keys.addEventListener("click", event => {
-  const { target } = event;
-  if (!target.matches("button")) {
-    return;
-  }
-  if (target.classList.contains("operator")) {
-    handleOperator(target.value);
+  "*": (firstOperand, secondOperand) => firstOperand * secondOperand,
+
+  "+": (firstOperand, secondOperand) => firstOperand + secondOperand,
+
+  "-": (firstOperand, secondOperand) => firstOperand - secondOperand,
+
+  "=": (firstOperand, secondOperand) => secondOperand
+};
+function resetCalculator() {
+  calculator.dsiplayValue = "0";
+  calculator.firstOperand = null;
+  calculator.waitingForSecondOperand = false;
+  calculator.operator = null;
+}
+const keys = document.getElementsByClassName("calculator-keys");
+for (var zes = 0; zes < keys.length; zes++) {
+  keys[zes].addEventListener("click", event => {
+    const { target } = event;
+    console.log(target, event);
+    if (!target.matches("button")) {
+      return;
+    }
+    if (target.classList.contains("operator")) {
+      handleOperator(target.value);
+      updateDisplay();
+      return;
+    }
+    if (target.classList.contains("all-clear")) {
+      resetCalculator();
+      updateDisplay();
+      return;
+    }
+
+    inputDigit(target.value);
     updateDisplay();
-    return;
-  }
-  if (target.classList.contains("all-clear")) {
-    return;
-  }
-
-  inputDigit(target.value);
-  updateDisplay();
-});
+  });
+}
